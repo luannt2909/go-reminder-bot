0e188751e3e71f5f74b0542cacae162f5b1eec21
luannt <luannt2@sendo.vn>
2023-12-06 20:59:48 +0700

    feat: hash password before saving

--- admin/server/auth.go
+++ admin/server/auth.go
@@ -7,6 +7,7 @@ import (
 	"go-reminder-bot/pkg/reminder"
 	"go-reminder-bot/pkg/token"
 	"go-reminder-bot/pkg/user"
+	"go-reminder-bot/pkg/util"
 	"net/http"
 	"time"
 )
@@ -14,21 +15,22 @@ import (
 func (h Handler) Login(c *gin.Context) {
 	ctx := c.Request.Context()
 	var req struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required"`
 	}
 	err := c.ShouldBind(&req)
 	if err != nil {
 		_ = c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
-	u, err := h.userStorage.Authenticate(ctx, req.Username, req.Password)
+	passwordHash := util.Hash256(req.Password)
+	u, err := h.userStorage.Authenticate(ctx, req.Username, passwordHash)
 	if err != nil && !errors.Is(err, user.ErrNotFound) {
 		_ = c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	if errors.Is(err, user.ErrNotFound) {
-		u, err = h.registerUser(c, req.Username, req.Password)
+		u, err = h.registerUser(c, req.Username, passwordHash)
 		if err != nil {
 			_ = c.AbortWithError(http.StatusInternalServerError, err)
 			return
